9b5ed86da6f4ca8af06cee7556061d5ccd66ba84
Brik94 <bkirkpa4@uncc.edu>
2019-12-08 20:57:08 -0500

    updated ssl setting

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { Pool } = require("pg");
 
 //Uncomment to use online database.
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
   ssl: true
 });
 
