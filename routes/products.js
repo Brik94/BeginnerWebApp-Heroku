07ca358837fb7294c3ca089792eea7461bec6765
Brik94 <bkirkpa4@uncc.edu>
2019-12-09 14:49:18 -0500

    added more logging

--- routes/products.js
+++ routes/products.js
@@ -2,20 +2,21 @@ module.exports = {
   getProducts: async (req, res) => {
     let query = "SELECT * FROM products;"; // query database to get all the products
 
-    // execute query
+    console.log("Attempting to execute the query.");
     await db.connect();
     await db.query(query, (error, result) => {
       if (error) {
+        console.log(error);
         res.redirect("/"); //if there's an error, redirect to home.
       }
 
-      //Convert database results to an array.
+      console.log("Converting database results to an array");
       var arr = [];
       for (var i = 0; i < result.rows.length; i++) {
         arr.push(result.rows[i]);
       }
 
-      //Pass the new array to our product view and render.
+      console.log("Passing the new array to our product view and render.");
       res.render("pages/products.ejs", {
         title: "This is the Product Page!",
         products: arr
